16e15c2a94c728fb6caacc4f9ab54155512fb9b7
khorasanireyhan <khorasanireyhane23@gmail.com>
2025-09-02 11:44:07 +0330

    **Exercise 1**

--- Program.cs
+++ Program.cs
@@ -5,11 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace csharp_exercises
+
+   //Extra exercises for7 C# beyond the textbook.
 {
     class Program
     {
+        /*تمرین ۱
+        توضیح: گرفتن ورودی سن کاربر و نمایش آن
+        */
         static void Main(string[] args)
         {
+            Console.WriteLine("enter your age");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("your age is" + age);
+
         }
     }
 }
