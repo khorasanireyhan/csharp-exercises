942e76c1e7b16444d070a198a34c361c4c194f47
khorasanireyhan <khorasanireyhane23@gmail.com>
2025-09-05 17:37:37 +0330

    Practice if statements: get number from user and print message

--- Class1.cs
+++ Class1.cs
@@ -1,12 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace csharp_exercises
 {
-    class Class1
+    // C# in easy way (book)_
+    class Exercise2
     {
+        // **First exercise of the book: the “if” section**
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your number:");
+            int number = int.Parse(Console.ReadLine());  // گرفتن ورودی از کاربر و تبدیل به عدد
+
+            if (number > 10)
+            {
+                Console.WriteLine("Hello world");
+            }
+            if (number < 10)
+            {
+                Console.WriteLine("Goodbye world");
+            }
+            if (number == 10)
+            {
+                Console.WriteLine("Your number is 10");
+            }
+
+        }
     }
 }
+
+
---
+++ reyhane.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_exercises
+
+   //Extra exercises for7 C# beyond the textbook.
+{
+    class reyhane
+    {
+       
+        static void Main(string[] args)
+        {
+          
+
+        }
+    }
+}
